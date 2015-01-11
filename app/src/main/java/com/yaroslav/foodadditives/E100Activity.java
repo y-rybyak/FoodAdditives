f6e4f5a43ba36b7dfa54c83d2b0a3ab7817d6e51
Yaroslav Rybyak <yaroslav.rybyak@ya.ru>
2015-01-11 23:21:24 +0200

    first commit

---
+++ app/src/main/java/com/yaroslav/foodadditives/E100Activity.java
@@ -0,0 +1,21 @@
+package com.yaroslav.foodadditives;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class E100Activity extends Activity {
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_e100);
+    }
+
+    public void E100ReturnClick(View view) {
+        Intent intent = new Intent(E100Activity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
