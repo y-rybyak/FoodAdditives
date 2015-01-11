983ea1e003e816f6a16f03e0f2468d9afbc4e029
Yaroslav Rybyak <yaroslav.rybyak@ya.ru>
2015-01-11 23:35:36 +0200

    Added E102

--- app/src/main/java/com/yaroslav/foodadditives/MainActivity.java
+++ app/src/main/java/com/yaroslav/foodadditives/MainActivity.java
@@ -1,42 +1,16 @@
 package com.yaroslav.foodadditives;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void ButtonE100Click(View view) {
@@ -46,6 +20,11 @@ public class MainActivity extends ActionBarActivity {
 
     public void ButtonE101Click(View view) {
         Intent intent = new Intent(MainActivity.this, E101Activity.class);
+        startActivity(intent);
+    }
+
+    public void ButtonE102Click(View view) {
+        Intent intent = new Intent(MainActivity.this, E102Activity.class);
         startActivity(intent);
     }
 }
