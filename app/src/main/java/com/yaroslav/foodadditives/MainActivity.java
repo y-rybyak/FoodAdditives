f7f6f0c4180862714db1beb485b8d8ff7a4438f9
Yaroslav Rybyak <yaroslav.rybyak@ya.ru>
2015-01-12 21:27:02 +0200

    fiing minor bugs

--- app/src/main/java/com/yaroslav/foodadditives/MainActivity.java
+++ app/src/main/java/com/yaroslav/foodadditives/MainActivity.java
@@ -24,96 +24,96 @@ public class MainActivity extends Activity {
     public void ButtonE123Click(View view) {
         Intent intent = new Intent(MainActivity.this, E123Activity.class);
         startActivity(intent);
-    }	
-	public void ButtonE127Click(View view) {
+    }
+    public void ButtonE127Click(View view) {
         Intent intent = new Intent(MainActivity.this, E127Activity.class);
         startActivity(intent);
     }
-	public void ButtonE128Click(View view) {
+    public void ButtonE128Click(View view) {
         Intent intent = new Intent(MainActivity.this, E128Activity.class);
         startActivity(intent);
     }
-	public void ButtonE142Click(View view) {
+    public void ButtonE142Click(View view) {
         Intent intent = new Intent(MainActivity.this, E142Activity.class);
         startActivity(intent);
     }
-	public void ButtonE154Click(View view) {
+    public void ButtonE154Click(View view) {
         Intent intent = new Intent(MainActivity.this, E154Activity.class);
         startActivity(intent);
     }
-	public void ButtonE173Click(View view) {
+    public void ButtonE173Click(View view) {
         Intent intent = new Intent(MainActivity.this, E173Activity.class);
         startActivity(intent);
     }
-	public void ButtonE180Click(View view) {
+    public void ButtonE180Click(View view) {
         Intent intent = new Intent(MainActivity.this, E180Activity.class);
         startActivity(intent);
     }
-	public void ButtonE216Click(View view) {
+    public void ButtonE216Click(View view) {
         Intent intent = new Intent(MainActivity.this, E216Activity.class);
         startActivity(intent);
     }
-	public void ButtonE217Click(View view) {
+    public void ButtonE217Click(View view) {
         Intent intent = new Intent(MainActivity.this, E217Activity.class);
         startActivity(intent);
     }
-	public void ButtonE240Click(View view) {
+    public void ButtonE240Click(View view) {
         Intent intent = new Intent(MainActivity.this, E240Activity.class);
         startActivity(intent);
     }
-	public void ButtonE388Click(View view) {
+    public void ButtonE388Click(View view) {
         Intent intent = new Intent(MainActivity.this, E388Activity.class);
         startActivity(intent);
     }
-	public void ButtonE389Click(View view) {
+    public void ButtonE389Click(View view) {
         Intent intent = new Intent(MainActivity.this, E389Activity.class);
         startActivity(intent);
     }
-	public void ButtonE424Click(View view) {
+    public void ButtonE424Click(View view) {
         Intent intent = new Intent(MainActivity.this, E424Activity.class);
         startActivity(intent);
     }
-	public void ButtonE425Click(View view) {
+    public void ButtonE425Click(View view) {
         Intent intent = new Intent(MainActivity.this, E425Activity.class);
         startActivity(intent);
     }
-	public void ButtonE512Click(View view) {
+    public void ButtonE512Click(View view) {
         Intent intent = new Intent(MainActivity.this, E512Activity.class);
         startActivity(intent);
     }
-	public void ButtonE537Click(View view) {
+    public void ButtonE537Click(View view) {
         Intent intent = new Intent(MainActivity.this, E537Activity.class);
         startActivity(intent);
     }
-	public void ButtonE557Click(View view) {
+    public void ButtonE557Click(View view) {
         Intent intent = new Intent(MainActivity.this, E557Activity.class);
         startActivity(intent);
     }
-	public void ButtonE912Click(View view) {
+    public void ButtonE912Click(View view) {
         Intent intent = new Intent(MainActivity.this, E912Activity.class);
         startActivity(intent);
     }
-	public void ButtonE914Click(View view) {
+    public void ButtonE914Click(View view) {
         Intent intent = new Intent(MainActivity.this, E914Activity.class);
         startActivity(intent);
     }
-	public void ButtonE916Click(View view) {
+    public void ButtonE916Click(View view) {
         Intent intent = new Intent(MainActivity.this, E916Activity.class);
         startActivity(intent);
     }
-	public void ButtonE917Click(View view) {
+    public void ButtonE917Click(View view) {
         Intent intent = new Intent(MainActivity.this, E917Activity.class);
         startActivity(intent);
     }
-	public void ButtonE918Click(View view) {
+    public void ButtonE918Click(View view) {
         Intent intent = new Intent(MainActivity.this, E918Activity.class);
         startActivity(intent);
     }
-	public void ButtonE919Click(View view) {
+    public void ButtonE919Click(View view) {
         Intent intent = new Intent(MainActivity.this, E919Activity.class);
         startActivity(intent);
     }
-	public void ButtonE922Click(View view) {
+    public void ButtonE922Click(View view) {
         Intent intent = new Intent(MainActivity.this, E922Activity.class);
         startActivity(intent);
     }
@@ -121,15 +121,15 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(MainActivity.this, E923Activity.class);
         startActivity(intent);
     }
-	public void ButtonE925Click(View view) {
+    public void ButtonE925Click(View view) {
         Intent intent = new Intent(MainActivity.this, E925Activity.class);
         startActivity(intent);
     }
-	public void ButtonE926Click(View view) {
+    public void ButtonE926Click(View view) {
         Intent intent = new Intent(MainActivity.this, E926Activity.class);
         startActivity(intent);
     }
-	public void ButtonE929Click(View view) {
+    public void ButtonE929Click(View view) {
         Intent intent = new Intent(MainActivity.this, E929Activity.class);
         startActivity(intent);
     }
