07eac21306c84046df1c48a10c3b0cbb50c635d8
Yaroslav Rybyak <yaroslav.rybyak@ya.ru>
2015-01-30 00:02:18 +0200

    more e5xx

--- app/src/main/java/com/yrybyak/foodadditives/E500E599Activity.java
+++ app/src/main/java/com/yrybyak/foodadditives/E500E599Activity.java
@@ -27,4 +27,51 @@ public class E500E599Activity extends Activity {
         startActivity(intent);
     }
 
+    public void ButtonE503Click(View view) {
+        Intent intent = new Intent(E500E599Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e503_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE504Click(View view) {
+        Intent intent = new Intent(E500E599Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e504_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE507Click(View view) {
+        Intent intent = new Intent(E500E599Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e507_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE508Click(View view) {
+        Intent intent = new Intent(E500E599Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e508_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE509Click(View view) {
+        Intent intent = new Intent(E500E599Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e509_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE510Click(View view) {
+        Intent intent = new Intent(E500E599Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e510_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE511Click(View view) {
+        Intent intent = new Intent(E500E599Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e511_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE512Click(View view) {
+        Intent intent = new Intent(E500E599Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e512_text));
+        startActivity(intent);
+    }
 }
