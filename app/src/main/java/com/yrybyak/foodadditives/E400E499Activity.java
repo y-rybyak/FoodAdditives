537e0a280fff0f4d9013b1bd73a852475c2f150d
Yaroslav Rybyak <yaroslav.rybyak@ya.ru>
2015-01-29 12:39:32 +0200

    e4xx

--- app/src/main/java/com/yrybyak/foodadditives/E400E499Activity.java
+++ app/src/main/java/com/yrybyak/foodadditives/E400E499Activity.java
@@ -1,7 +1,9 @@
 package com.yrybyak.foodadditives;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class E400E499Activity extends Activity {
 
@@ -12,11 +14,16 @@ public class E400E499Activity extends Activity {
         themeUtils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_e400_e499);
     }
-/*
-    public void ButtonE200Click(View view) {
-        Intent intent = new Intent(E300E399Activity.this, EActivity.class);
-        intent.putExtra("text", getResources().getString(R.string.e200_text));
+
+    public void ButtonE401Click(View view) {
+        Intent intent = new Intent(E400E499Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e401_text));
         startActivity(intent);
     }
-*/
+
+    public void ButtonE406Click(View view) {
+        Intent intent = new Intent(E400E499Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e406_text));
+        startActivity(intent);
+    }
 }
