4d9a890f67791b47a144e31941f5be8a7c0885d0
Yaroslav Rybyak <yaroslav.rybyak@ya.ru>
2015-01-31 13:59:25 +0200

    e6xx

--- app/src/main/java/com/yrybyak/foodadditives/E600E699Activity.java
+++ app/src/main/java/com/yrybyak/foodadditives/E600E699Activity.java
@@ -1,7 +1,9 @@
 package com.yrybyak.foodadditives;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class E600E699Activity extends Activity {
 
@@ -12,11 +14,16 @@ public class E600E699Activity extends Activity {
         themeUtils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_e600_e699);
     }
-/*
-    public void ButtonE200Click(View view) {
-        Intent intent = new Intent(E300E399Activity.this, EActivity.class);
-        intent.putExtra("text", getResources().getString(R.string.e200_text));
+
+    public void ButtonE621Click(View view) {
+        Intent intent = new Intent(E600E699Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e621_text));
         startActivity(intent);
     }
-*/
+
+    public void ButtonE627Click(View view) {
+        Intent intent = new Intent(E600E699Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e627_text));
+        startActivity(intent);
+    }
 }
