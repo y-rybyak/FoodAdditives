e1c351f7ef1a7808ad4c9defe7912464e116fa04
Yaroslav Rybyak <yaroslav.rybyak@ya.ru>
2015-01-31 16:49:30 +0200

    e1xxx

--- app/src/main/java/com/yrybyak/foodadditives/E1000E1999Activity.java
+++ app/src/main/java/com/yrybyak/foodadditives/E1000E1999Activity.java
@@ -1,7 +1,9 @@
 package com.yrybyak.foodadditives;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class E1000E1999Activity extends Activity {
 
@@ -12,11 +14,70 @@ public class E1000E1999Activity extends Activity {
         themeUtils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_e1000_e1999);
     }
-/*
-    public void ButtonE200Click(View view) {
-        Intent intent = new Intent(E300E399Activity.this, EActivity.class);
-        intent.putExtra("text", getResources().getString(R.string.e200_text));
+
+    public void ButtonE1100Click(View view) {
+        Intent intent = new Intent(E1000E1999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e1100_text));
         startActivity(intent);
     }
-*/
+
+    public void ButtonE1104Click(View view) {
+        Intent intent = new Intent(E1000E1999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e1104_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE1105Click(View view) {
+        Intent intent = new Intent(E1000E1999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e1105_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE1400Click(View view) {
+        Intent intent = new Intent(E1000E1999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e1400_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE1404Click(View view) {
+        Intent intent = new Intent(E1000E1999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e1404_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE1422Click(View view) {
+        Intent intent = new Intent(E1000E1999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e1442_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE1442Click(View view) {
+        Intent intent = new Intent(E1000E1999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e1442_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE1450Click(View view) {
+        Intent intent = new Intent(E1000E1999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e1450_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE1510Click(View view) {
+        Intent intent = new Intent(E1000E1999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e1510_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE1518Click(View view) {
+        Intent intent = new Intent(E1000E1999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e1518_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE1520Click(View view) {
+        Intent intent = new Intent(E1000E1999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e1520_text));
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/yrybyak/foodadditives/E900E999Activity.java
+++ app/src/main/java/com/yrybyak/foodadditives/E900E999Activity.java
@@ -26,4 +26,118 @@ public class E900E999Activity extends Activity {
         intent.putExtra("text", getResources().getString(R.string.e912_text));
         startActivity(intent);
     }
+
+    public void ButtonE913Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e913_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE914Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e914_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE916Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e916_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE917Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e917_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE918Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e918_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE919Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e919_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE922Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e922_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE923Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e923_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE925Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e925_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE926Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e926_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE929Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e929_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE950Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e950_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE951Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e951_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE952Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e952_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE953Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e953_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE954Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e954_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE965Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e965_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE967Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e967_text));
+        startActivity(intent);
+    }
+
+    public void ButtonE999Click(View view) {
+        Intent intent = new Intent(E900E999Activity.this, EActivity.class);
+        intent.putExtra("text", getResources().getString(R.string.e999_text));
+        startActivity(intent);
+    }
 }
